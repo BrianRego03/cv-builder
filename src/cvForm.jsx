0235586e6b527577c74c77a81c189d619733a0e6
Brian rego <brian.rego03@gmail.com>
2025-02-16 23:28:59 +0530

    Add buttons to hide sections of form

--- src/cvForm.jsx
+++ src/cvForm.jsx
@@ -1,15 +1,52 @@
-
+// import { useState } from "react";
 
 
 
 function CvForm(){
 
+    function showPersonalDetails(){
+        const personalDetails=document.getElementById("personalDetailsSection");
+        if(personalDetails.style.display=="none"){
+            personalDetails.style.display="block"
+
+        }
+        else{
+            personalDetails.style.display="none"
+        }
+        
+    }
+    function toggleEducation(){
+        const educationDetails=document.getElementById("educationSection");
+        if(educationDetails.style.display=="none"){
+            educationDetails.style.display="block"
+
+        }
+        else{
+            educationDetails.style.display="none"
+        }
+
+    }
+    function toggleExperience(identity){
+        const experienceSection=document.getElementById(identity);
+        if(experienceSection.style.display=="none"){
+            experienceSection.style.display="block"
+
+        }
+        else{
+            experienceSection.style.display="none"
+        }
+
+    }
+
+
 
     return(
         <>
             <form action={"#"} method="POST">
-                <button type="button">Personal Details</button>
-                <fieldset>
+                <div>
+                    <button type="button" onClick={showPersonalDetails}>Personal Details</button>
+                </div>
+                <fieldset className="personalDetails" id="personalDetailsSection">
                     <legend>Personal Information</legend>
                     <div><label htmlFor="name">Name:</label></div>
                     <input type="text" id="name"></input>
@@ -22,31 +59,40 @@ function CvForm(){
                     
 
                 </fieldset>
-                <fieldset>
-                <legend>Education</legend>
-                <div><label htmlFor="college">College name:</label></div>
-                <div><input type="text" id="college"></input></div>
-                <div><label htmlFor="degree">Degree:</label></div>
-                <div><input type="text" id="degree"></input></div>
-                <div><label htmlFor="startDate">Start Date:</label></div>
-                <div><input type="date" id="startDate"></input></div>
-                <div><label htmlFor="endDate">End Date:</label></div>
-                <div><input type="date" id="endDate"></input></div>
-                <div><label htmlFor="cgpa">CGPA:</label></div>
-                <div><input type="text" id="cgpa"></input></div>
+                <div>
+                    <button type="button" onClick={toggleEducation}>Education</button>
+
+                </div>
+
+                <fieldset id="educationSection">
+                    <legend>Education</legend>
+                    <div><label htmlFor="college">College name:</label></div>
+                    <div><input type="text" id="college"></input></div>
+                    <div><label htmlFor="degree">Degree:</label></div>
+                    <div><input type="text" id="degree"></input></div>
+                    <div><label htmlFor="startDate">Start Date:</label></div>
+                    <div><input type="date" id="startDate"></input></div>
+                    <div><label htmlFor="endDate">End Date:</label></div>
+                    <div><input type="date" id="endDate"></input></div>
+                    <div><label htmlFor="cgpa">CGPA:</label></div>
+                    <div><input type="text" id="cgpa"></input></div>
 
                 </fieldset>
+                <div>
+                    <button type="button" 
+                    onClick={()=>{toggleExperience("experienceSection")}}>Experience</button>
 
-                <fieldset>
+                </div>
+                <fieldset id="experienceSection">
                 <legend>Experience</legend>
                 <div><label htmlFor="jobTitle">Job Title:</label></div>
                 <div><input type="text" id="jobTitle"></input></div>
                 <div><label htmlFor="company">Company:</label></div>
                 <div><input type="text" id="company"></input></div>
-                <div><label htmlFor="jstartDate">Start Date:</label></div>
-                <div><input type="month" id="startDate"></input></div>
-                <div><label htmlFor="endDate">End Date:</label></div>
-                <div><input type="month" id="endDate"></input></div>
+                <div><label htmlFor="jobStartDate">Start Date:</label></div>
+                <div><input type="month" id="jobStartDate"></input></div>
+                <div><label htmlFor="JobEndDate">End Date:</label></div>
+                <div><input type="month" id="JobEndDate"></input></div>
                 <div><label htmlFor="jobDetails">Job Details:</label></div>
                 <div><input type="text" id="jobDetails"></input></div>
                 <div><label htmlFor="jobLocation">Job Location:</label></div>
