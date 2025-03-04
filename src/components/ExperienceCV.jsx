1ce57f20f53c2103dfe129a06090f31c2ab9aa63
brianrego03 <brian.rego03@gmail.com>
2025-03-04 13:15:11 +0530

    Add date formatter to experience component

--- src/components/ExperienceCV.jsx
+++ src/components/ExperienceCV.jsx
@@ -1,1 +1,16 @@
 /* eslint-disable react/prop-types */
+
+import "../styles/ExperienceCV.css"
+
+function ExperienceComponent(index,person){
+    function dateFormatter(dateString){
+        const dateObj = new Date(dateString);
+        const options = { year: 'numeric', month: 'long' };
+        const formattedDate = dateObj.toLocaleDateString('en-US', options);
+        return formattedDate;
+
+
+
+    }
+
+}
